ae5aa7bce4ea4f21bd7563e5ec06ee6b2070e510
llc6wh <llc6wh@virginia.edu>
2018-09-06 17:15:45 -0400

    initial commit

---
+++ app/src/main/java/bucketlist/cs4720/cs/virginia/edu/bucketlist/BucketListActivity.java
@@ -0,0 +1,14 @@
+package bucketlist.cs4720.cs.virginia.edu.bucketlist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class listOfActivities extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_of_activities);
+    }
+
+}
